0913cfb49e8cd2671556987968df0a5c9721af59
Robert Le Monds <robertlemondsortiz@me.com>
2017-01-18 21:54:12 -0600

    had too much trouble adding knockout reset.

--- js/test.js
+++ js/test.js
@@ -1,103 +1,24 @@
-var map;
-      // Create a new blank array for all the listing markers.
-      var self = this;
-      // Google Maps style contribution of Snazzymaps.com
-      // Made by JulienJ - https://snazzymaps.com/style/70868/pokemon-go-style
-      var style = [{
-        "featureType": "all",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#c4e8c7"
-        }]
-      }, {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "##000000"
-        }]
-      }, {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#a9daba"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#b6a7a3"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#f3f5e7"
-        }, {
-          "weight": "0.01"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "simplified"
-        }]
-      }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "weight": "0.01"
-        }]
-      }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#b6a7a3"
-        }]
-      }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#f3f5e7"
-        }]
-      }, {
-        "featureType": "road.local",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#b6a7a3"
-        }]
-      }, {
-        "featureType": "road.local",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#f3f5e7"
-        }, {
-          "weight": "0.01"
-        }]
-      }, {
-        "featureType": "transit.line",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#a1a0a0"
-        }]
-      }, {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#65cee2"
-        }]
-      }, {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*
       var markers = [];
         //Message for Google Maps failing.
   this.mapRequestTimeout = setTimeout(function() {
@@ -126,7 +47,6 @@ var map;
           var position = locations[i].location;
           var title = locations[i].title;
           // Create a marker per location, and put into markers array.
-          var address = locations[i].address;
           var image = {
             url: 'img/pokemon-marker.png',
             scaledSize: new google.maps.Size(20, 32),
@@ -159,7 +79,7 @@ var map;
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.setContent('<div>' + marker.title + marker.address + '</div>');
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
@@ -183,3 +103,5 @@ var map;
           markers[i].setMap(null);
         }
       }
+
+      */
