9fc11b5c58d04de0fb3945073037dea4fa62c189
Robert Le Monds <robertlemondsortiz@me.com>
2017-01-31 21:00:20 -0600

    added styles, knockoutjs, YELP api, minifed app.js, fixed model locations, added animations to marker, added filter, responsive mobile, styles added.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# udacity-pokestopmap
+# Pokemon Go Mini-Map
+
+##Get Started
+
+1. Either go to the github pages [site](https://robertlemonds.github.io/udacity-pokestopmap/) or download repository.
+
+2. Once you are on the site or the repository is downloaded you can begin using the Pokemon Go Mini-Map by typing in the search bar (either exact keyword or by pokestop/gym).
+
+3. Alternatively you can click the locations on the list and an animation for the marker will start bounce to show where it is on the map.
+
+4. Clicking a Marker on the map will pull data from Yelps API giving you a review of the local business there if you want to enjoy yourself while you are out and about.
+
+5. When an infowindow is opened via clicking or tapping (mobile) the link will take you to the Yelp page where you can find more information.
+
+
--- js/model.js
+++ js/model.js
@@ -1,66 +1,68 @@
-// These are the Pokestop Locations near Northstar Mall San Antonio, TX.
-
-        var locations = [{
-          title: 'Slackers',
-          address: '126 W Rector, San Antonio, TX 78216',
-          location: {
-            lat: 29.5161377,
-            lng: -98.4978737, 
-          }
-        }, {
-          title: 'Alamo Drafthouse Cinema',
-          address: 'Park North Shopping Center, 618 NW Loop 410, San Antonio, TX 78216',
-          location: {
-            lat: 29.5181499,
-            lng: -98.5051367,           
-          }
-        }, {
-          title: 'Jims Restaurant',
-          address: 'Park North Shopping Center, San Antonio, TX 78216',
-          location: {
-            lat: 29.5197297,
-            lng: -98.5081427,
-          }
-        },  {
-          title: 'McFinnigans Pub',
-          address: 'Park North Shopping Center, San Antonio, TX 78216',
-          location: {
-            lat: 29.5236395,
-            lng: -98.5062881,
-          }
-        },  {
-          title: 'Alien Worlds',
-          address: 'Park North Shopping Center, San Antonio, TX 78216',
-          location: {
-            lat: 29.5057626,
-            lng: -98.500715,
-          }
-        },  {
-          title: 'San Antonio Airport',
-          location: {
-            lat: 29.5311973,
-            lng: -98.4705371,
-            address: 'Park North Shopping Center, San Antonio, TX 78216'
-          }
-        },  {
-          title: 'Thanks for Vaping',
-          location: {
-            lat: 29.5156303,
-            lng: -98.5019474,
-            address: 'Park North Shopping Center, San Antonio, TX 78216'
-          }
-        },  {
-          title: 'Diversions Game Room',
-          location: {
-            lat: 29.530484,
-            lng: -98.4987076,
-            address: '9900 San Pedro Ave, San Antonio, TX 78216' 
-          }
-        }, {
-          title: 'Freebirds',
-          location: {
-            lat: 29.5223566,
-            lng: -98.4956672,
-            address: '125 NW Loop 410 #440, San Antonio, TX 78216' 
-          }
-        }];
+// ---- Model for Site or Locations Array ---- //
+//Pokestops and Gyms in the North Star Mall area of San Antonio, TX
+var pokestopLocations = [
+  {
+    name: 'Slackers',
+    lat: 29.5161377,
+    lng: -98.4978737,
+    address: '126 W Rector, San Antonio, TX 78216',
+    category: 'PokeStop'
+  },
+  {
+    name: 'Alamo Drafthouse Cinema - Park North',
+    lat: 29.5181499,
+    lng: -98.5051367,
+    address: '618 NW Loop 410 San Antonio, TX 78216',
+    category: 'PokeStop'
+  },
+  {
+    name: 'Diversions Game Room',
+    lat: 29.530484,
+    lng: -98.4987076,
+    address: '9900 San Pedro Ave, San Antonio, TX 78216',
+    category: 'PokeStop'
+  },
+  {
+    name: 'Jims Restaurant',
+    lat: 29.5197297,
+    lng: -98.5081427,
+    address: '842 NW Loop 410 San Antonio, TX 78216',
+    category: 'Gym'
+  },
+  {
+    name: 'McFinnigans Pub',
+    lat: 29.5236395,
+    lng: -98.5062881,
+    address: '7210 Blanco Rd San Antonio, TX 78216',
+    category: 'Gym'
+  },
+  {
+    name: 'Alien Worlds',
+    lat: 29.5057626,
+    lng: -98.500715,
+    address: '6900 San Pedro Ave San Antonio, TX 78216',
+    category: 'Gym'
+  },
+  {
+    name: 'Thanks for Vaping',
+    lat: 29.5156303,
+    lng: -98.5019474,
+    address: '7303 San Pedro Ave Ste 101 San Antonio, TX 78216',
+    category: 'Gym'
+  },
+  {
+    name: 'San Antonio Airport',
+    lat: 29.5311973,
+    lng: -98.4705371,
+    address: '9800 Airport Blvd San Antonio, TX 78216',
+    category: 'Gym'
+  },
+  {
+    name: 'Freebirds World Burrito',
+    lat: 29.5223566,
+    lng: -98.4956672,
+    address: '125 NW Loop 410 San Antonio, TX 78216',
+    category: 'PokeStop'
+  },
+  
+];
